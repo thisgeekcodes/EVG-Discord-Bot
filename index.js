1b626855691de535c6d47a27fc170dc21091daa9
This Geek Codes <bachmanmichael@gmail.com>
2025-01-31 22:31:53 -0600

    checkMessage

--- index.js
+++ index.js
@@ -7,20 +7,9 @@ const client = new Client({
   ],
 });
 
-client.on("messageCreate", (message) => {
-  // Check if the message is in the specific channel
-  if (message.channelId === "1326421629441540096") {
-    // Check if the message has attachments (media)
-    if (!message.attachments.size) {
-      message.delete();
-      // Optionally send a message to the user explaining why
-      message.author.send(
-        "Your message in #highlights was deleted because it did not contain any media. Picture/Video"
-      );
-    }
-  }
-});
-
 const config = require("./config.json");
+const { checkMessage } = require("./message.js");
+
+checkMessage();
 
 client.login(config.BOT_TOKEN);
---
+++ message.js
@@ -0,0 +1,19 @@
+function checkMessage() {
+  client.on("messageCreate", (message) => {
+    // Check if the message is in the specific channel
+    if (message.channelId === "1326421629441540096") {
+      // Check if the message has attachments (media)
+      if (!message.attachments.size) {
+        message.delete();
+        // Optionally send a message to the user explaining why
+        message.author.send(
+          "Your message in #highlights was deleted because it did not contain any media. Picture/Video"
+        );
+      }
+    }
+  });
+}
+
+module.exports = {
+  checkMessage: checkMessage,
+};
