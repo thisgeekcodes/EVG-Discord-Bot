158a538d5776ca51616ce12825b21903a5d2d83c
This Geek Codes <bachmanmichael@gmail.com>
2025-02-02 09:41:51 -0600

    last

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const { Client, GatewayIntentBits, Events } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const { Client, Collection, GatewayIntentBits, Events } = require("discord.js");
 
 const config = require("./config.json");
 
@@ -11,6 +14,41 @@ const client = new Client({
   ],
 });
 
+// Commands
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, (interaction) => {
+  console.log(interaction);
+});
+
+client.on(Events.InteractionCreate, (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  console.log(interaction);
+});
+
+// Client Ready
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
