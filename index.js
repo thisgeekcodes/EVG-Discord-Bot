9aa3aab5c1f54c41ff03ae1729a87be64181ae98
This Geek Codes <bachmanmichael@gmail.com>
2025-02-02 09:47:56 -0600

    1

--- index.js
+++ index.js
@@ -14,40 +14,6 @@ const client = new Client({
   ],
 });
 
-// Commands
-client.commands = new Collection();
-
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
-  }
-}
-
-client.on(Events.InteractionCreate, (interaction) => {
-  console.log(interaction);
-});
-
-client.on(Events.InteractionCreate, (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  console.log(interaction);
-});
-
 // Client Ready
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
