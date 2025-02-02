19fc1d504dd2e6ac311c8468aa9919655fae964e
This Geek Codes <bachmanmichael@gmail.com>
2025-02-02 09:20:05 -0600

    Latest

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+
+const fs = require("fs");
+const path = require("path");
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -7,9 +11,35 @@ const client = new Client({
   ],
 });
 
-const config = require("./config.json");
-const mes = require("./message.js");
+client.commands = new Collection();
 
-mes.checkMessage;
+// Load command files
+const commandFiles = fs
+  .readdirSync(path.join(__dirname, "commands"))
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
+const config = require("./config.json");
 
 client.login(config.BOT_TOKEN);
