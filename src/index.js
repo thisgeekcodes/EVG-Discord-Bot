7267e7a167e7d444e7e83b3bb3128240c9e2b32a
This Geek Codes <bachmanmichael@gmail.com>
2025-02-02 12:10:58 -0600

    Updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 const fs = require("node:fs");
 const path = require("node:path");
 
 const { Client, Collection, GatewayIntentBits, Events } = require("discord.js");
 
-const config = require("./config.json");
+const config = require("../config.json");
 
 // Create a new client instance
 const client = new Client({
@@ -19,4 +21,4 @@ client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.login(config.BOT_TOKEN);
+client.login(process.env.TOKEN);
